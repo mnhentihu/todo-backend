6596e85b8691415853231ad7c6bdd810d2042ae9
Muhammad Natsir hentihu <hentihu.natsir@gmail.com>
2024-02-17 13:22:36 +0700

    Add Resource and change todo controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TodoResource;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     //
+    public function index()
+    {
+        //get posts
+        $posts = Todo::latest();
+
+        //return collection of posts as a resource
+        return new TodoResource(true, 'List Data Todo', $posts);
+    }
 }
